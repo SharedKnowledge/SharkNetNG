bef58730be6acc98eebbcb565ad0004509ac28a2
Mario Neises <git@mnio.net>
2016-01-14 01:57:44 +0100

    dynamic msg size

--- app/src/main/java/de/htw_berlin/sharkandroidstack/modules/nfc/NfcBenchmarkFragment.java
+++ app/src/main/java/de/htw_berlin/sharkandroidstack/modules/nfc/NfcBenchmarkFragment.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import de.htw_berlin.sharkandroidstack.R;
@@ -27,6 +28,7 @@ public class NfcBenchmarkFragment extends Fragment {
     //TODO: migrate chat
 
     NfcMainActivity activity;
+    TextView msgLengthOutput;
 
     MyReaderCallback readerCallback;
     MyStartButtonClickListener buttonClickListener;
@@ -35,7 +37,8 @@ public class NfcBenchmarkFragment extends Fragment {
     final OnMessageSend benchmarkSource = new OnMessageSend() {
         @Override
         public byte[] getNextMessage() {
-            byte[] message = Utils.generateRandomString(30).getBytes();
+            int length = Integer.valueOf(msgLengthOutput.getText().toString());
+            byte[] message = Utils.generateRandomString(length).getBytes();
             resultAdapter.addMessageOut(message);
             return message;
         }
@@ -59,20 +62,43 @@ public class NfcBenchmarkFragment extends Fragment {
         }
     };
 
+    final SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            msgLengthOutput.setText(progress);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        resultAdapter = new MyResultAdapter(this.getActivity());
-
         final View root = inflater.inflate(R.layout.module_nfc_benchmark_fragment, container, false);
-        final ProgressBar progressBar = (ProgressBar) root.findViewById(R.id.activity_nfc_benchmark_progress);
-        final ListView results = (ListView) root.findViewById(R.id.activity_nfc_benchmark_results);
-        results.setAdapter(resultAdapter);
-
-        final TextView description = (TextView) root.findViewById(R.id.activity_nfc_benchmark_description);
-        description.setText(Html.fromHtml(getString(R.string.activity_nfc_benchmark_description)));
-        final Button startButton = (Button) root.findViewById(R.id.activity_nfc_benchmark_button_start);
 
         activity = (NfcMainActivity) NfcBenchmarkFragment.this.getActivity();
+        resultAdapter = new MyResultAdapter(this.getActivity());
+
+        msgLengthOutput = (TextView) root.findViewById(R.id.activity_nfc_benchmark_msg_length_output);
+        final SeekBar msgLengthInput = (SeekBar) root.findViewById(R.id.activity_nfc_benchmark_msg_length_input);
+        msgLengthInput.setOnSeekBarChangeListener(seekBarChangeListener);
+        msgLengthInput.setProgress(512);
+
+        final ProgressBar progressBar = (ProgressBar) root.findViewById(R.id.activity_nfc_benchmark_progress);
+        final ListView results = (ListView) root.findViewById(R.id.activity_nfc_benchmark_results);
+
+        final TextView description = (TextView) root.findViewById(R.id.activity_nfc_benchmark_description);
+        final Button startButton = (Button) root.findViewById(R.id.activity_nfc_benchmark_button_start);
+
+        results.setAdapter(resultAdapter);
+        description.setText(Html.fromHtml(getString(R.string.activity_nfc_benchmark_description)));
 
         buttonClickListener = new MyStartButtonClickListener(progressBar, results, description, benchmarkSource, activity, startButton);
         startButton.setOnClickListener(buttonClickListener);
