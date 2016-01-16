fa7e7e5e6c95957101b8469bfe3c846dd1a2ea92
Mario Neises <git@mnio.net>
2016-01-16 10:35:57 +0100

    working demo(5)

--- app/src/main/java/de/htw_berlin/sharkandroidstack/modules/nfc2/NfcActivity.java
+++ app/src/main/java/de/htw_berlin/sharkandroidstack/modules/nfc2/NfcActivity.java
@@ -1,7 +1,11 @@
 package de.htw_berlin.sharkandroidstack.modules.nfc2;
 
+import android.annotation.TargetApi;
 import android.graphics.Color;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.IsoDep;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,9 +14,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import de.htw_berlin.sharkandroidstack.R;
+import de.htw_berlin.sharkandroidstack.Utils;
 import de.htw_berlin.sharkandroidstack.android.ParentActivity;
+import de.htw_berlin.sharkandroidstack.modules.nfc2.hce.IsoDepTransceiver;
+import de.htw_berlin.sharkandroidstack.sharkFW.protocols.nfc.OnMessageReceived;
+import de.htw_berlin.sharkandroidstack.sharkFW.protocols.nfc.OnMessageSend;
+import de.htw_berlin.sharkandroidstack.sharkFW.protocols.nfc.SmartCardEmulationService;
 
-public abstract class NfcActivity extends ParentActivity {
+public class NfcActivity extends ParentActivity {
 
     private final View.OnClickListener exitOnClickListener = new View.OnClickListener() {
         @Override
@@ -75,7 +84,92 @@ public abstract class NfcActivity extends ParentActivity {
         }
     }
 
-    abstract void prepareSending(EditText input, NfcAdapter nfcAdapter);
+    StringBuilder outputStringBuilder = new StringBuilder();
 
-    abstract void prepareReceiving(TextView output, NfcAdapter nfcAdapter);
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    void prepareSending(EditText input, NfcAdapter nfcAdapter) {
+        SmartCardEmulationService.setSource(new OnMessageSend() {
+            @Override
+            public byte[] getNextMessage() {
+                byte[] message = Utils.generateRandomString(512).getBytes();
+                System.out.println("mario: send " + new String(message));
+                return message;
+            }
+
+            @Override
+            public void onDeactivated(int reason) {
+                System.out.println("mario: deactivated " + reason);
+            }
+        });
+        nfcAdapter.disableReaderMode(this);
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        final NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter != null) nfcAdapter.disableReaderMode(this);
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    void prepareReceiving(final TextView output, NfcAdapter nfcAdapter) {
+        // http://stackoverflow.com/questions/27939030/alternative-way-for-enablereadermode-to-work-with-android-apis-lesser-than-19
+        final OnMessageReceived onMessageReceived = new OnMessageReceived() {
+            @Override
+            public void onMessage(final byte[] message) {
+                System.out.println("mario: in " + new String(message));
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        outputStringBuilder.append(new String(message));
+                        output.setText(outputStringBuilder.toString());
+                    }
+                });
+            }
+
+            @Override
+            public void onError(Exception exception) {
+                exception.printStackTrace();
+                onMessage(("Finished with error: " + exception.getMessage()).getBytes());
+            }
+
+            @Override
+            public void tagLost(Tag tag) {
+                System.out.println("mario: tag lost");
+            }
+
+            @Override
+            public void newTag(Tag tag) {
+                System.out.println("mario: new tag");
+            }
+        };
+
+        final NfcAdapter.ReaderCallback readerCallback = new NfcAdapter.ReaderCallback() {
+            @Override
+            public void onTagDiscovered(Tag tag) {
+                System.out.println("mario new tag");
+                IsoDep isoDep = IsoDep.get(tag);
+                if (isoDep == null) {
+                    return;
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        outputStringBuilder = new StringBuilder();
+                        output.setText(outputStringBuilder.toString());
+                    }
+                });
+
+                IsoDepTransceiver transceiver = new IsoDepTransceiver(isoDep, onMessageReceived);
+                Thread thread = new Thread(transceiver);
+                thread.start();
+            }
+        };
+
+        final int flags = NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK;
+        nfcAdapter.enableReaderMode(this, readerCallback, flags, null);
+    }
 }
