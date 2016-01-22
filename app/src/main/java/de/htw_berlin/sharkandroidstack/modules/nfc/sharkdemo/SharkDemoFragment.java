a1cca70201a0d383d0d8c364eaba87d3515bdf7a
Mario Neises <git@mnio.net>
2016-01-22 13:47:21 +0100

    basic view for demo

--- app/src/main/java/de/htw_berlin/sharkandroidstack/modules/nfc/sharkdemo/SharkDemoFragment.java
+++ app/src/main/java/de/htw_berlin/sharkandroidstack/modules/nfc/sharkdemo/SharkDemoFragment.java
@@ -5,13 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import de.htw_berlin.sharkandroidstack.AndroidUtils;
 import de.htw_berlin.sharkandroidstack.R;
 
 public class SharkDemoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View root = inflater.inflate(R.layout.module_nfc_benchmark_fragment, container, false);
+        final View root = inflater.inflate(R.layout.module_nfc_sharkdemo_fragment, container, false);
+
+        final TextView ownerInformation = (TextView) root.findViewById(R.id.activity_nfc_sharkdemo_owner_id);
+        ownerInformation.setText("Your device id, used as KB owner: " + AndroidUtils.deviceId + "\n\nAdd Context information below:");
         return root;
     }
 
