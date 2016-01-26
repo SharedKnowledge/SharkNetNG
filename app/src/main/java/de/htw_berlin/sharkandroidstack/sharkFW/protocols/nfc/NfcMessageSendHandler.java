ba644c9218e1086cb24d41df7d0ee1a1699563bf
Mario Neises <git@mnio.net>
2016-01-26 13:57:34 +0100

    synchronized flag added

--- app/src/main/java/de/htw_berlin/sharkandroidstack/sharkFW/protocols/nfc/NfcMessageSendHandler.java
+++ app/src/main/java/de/htw_berlin/sharkandroidstack/sharkFW/protocols/nfc/NfcMessageSendHandler.java
@@ -6,18 +6,13 @@ import java.util.Arrays;
  * Created by mn-io on 25.01.2016.
  */
 public class NfcMessageSendHandler implements OnMessageSend {
-    byte[] byteBuffer = new byte[0];
+    byte[] byteBuffer = null;
     private int size;
+    private final Object lock = new Object();
 
     @Override
     public byte[] getNextMessage() {
         final byte[] data = getBytesFromBuffer(size);
-        if (data != null) {
-            System.out.println("mario: sending " + Arrays.toString(data));
-            System.out.println("mario: sending " + new String(data));
-        }
-
-        //TODO: clear after sending
         return data;
     }
 
@@ -32,23 +27,26 @@ public class NfcMessageSendHandler implements OnMessageSend {
     }
 
     public void setData(byte[] data) {
-        //TODO: check if data is not overwritten
-        if (byteBuffer != null && byteBuffer.length > 0) {
-            throw new IllegalStateException("Buffer not empty. Data loss on attempt to overwrite existing data");
+        synchronized (lock) {
+            if (byteBuffer != null && byteBuffer.length > 0) {
+                throw new IllegalStateException("Buffer not empty. Data loss on attempt to overwrite existing data");
+            }
+            this.byteBuffer = data;
         }
-        this.byteBuffer = data;
     }
 
     byte[] getBytesFromBuffer(int maxLength) {
-        if (byteBuffer == null || 0 == byteBuffer.length) {
-            byteBuffer = null;
-            return null;
+        synchronized (lock) {
+            if (byteBuffer == null || 0 == byteBuffer.length) {
+                byteBuffer = null;
+                return null;
+            }
+
+            int length = Math.min(byteBuffer.length, maxLength);
+            final byte[] currentBuffer = Arrays.copyOfRange(byteBuffer, 0, length);
+
+            byteBuffer = Arrays.copyOfRange(byteBuffer, length, byteBuffer.length);
+            return currentBuffer;
         }
-
-        int length = Math.min(byteBuffer.length, maxLength);
-        final byte[] currentBuffer = Arrays.copyOfRange(byteBuffer, 0, length);
-
-        byteBuffer = Arrays.copyOfRange(byteBuffer, length, byteBuffer.length);
-        return currentBuffer;
     }
 }
