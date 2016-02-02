760f929c10acf72d6fb96b0e4e0711bc7cf35fab
Michael Schwarz <schwarzm90@gmail.com>
2016-02-02 18:08:27 +0100

    Implement Stub- and ControllerListener.

--- app/src/main/java/de/htw_berlin/sharkandroidstack/sharkFW/protocols/wifidirect/CommunicationManager.java
+++ app/src/main/java/de/htw_berlin/sharkandroidstack/sharkFW/protocols/wifidirect/CommunicationManager.java
@@ -1,20 +1,63 @@
 package de.htw_berlin.sharkandroidstack.sharkFW.protocols.wifidirect;
 
+import android.annotation.TargetApi;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Build;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by micha on 28.01.16.
  */
-public class CommunicationManager {
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+public class CommunicationManager implements WifiP2pManager.DnsSdTxtRecordListener, WifiDirectStreamStub.Status{
 
-    private static CommunicationManager instance = null;
+    interface StubController{
+        public void onStubStart();
+        public void onStubStop();
+        public void onStubRestart();
+    }
+
+    interface ControllerActions{
+        public void onNewPeer(List<WifiDirectPeer> peers);
+        public void onConnect(WifiDirectPeer peer);
+        public void onDisconnect(WifiDirectPeer peer);
+    }
+
+    private static CommunicationManager instance = new CommunicationManager();
+    private List<WifiDirectPeer> peers = new LinkedList<>();
+    private ControllerActions controllerActionsListener;
+    private StubController stubControllerListener;
 
     public CommunicationManager() {}
 
-    public CommunicationManager getInstance(){
-        if(instance==null){
-            this.instance = new CommunicationManager();
-        }
+    public static CommunicationManager getInstance(){
         return instance;
     }
 
+    public void setControllerActionsListener(ControllerActions controllerActionsListener) {
+        this.controllerActionsListener = controllerActionsListener;
+    }
+
+    public void setStubControllerListener(StubController stubControllerListener) {
+        this.stubControllerListener = stubControllerListener;
+    }
+
+    @Override
+    public void onDnsSdTxtRecordAvailable(String fullDomainName, Map<String, String> txtRecordMap, WifiP2pDevice srcDevice) {
+        WifiDirectPeer newPeer = new WifiDirectPeer(srcDevice, txtRecordMap);
+        if(!this.peers.contains(newPeer)){
+            this.peers.add(newPeer);
+        }
+        controllerActionsListener.onNewPeer(peers);
+    }
+
+    @Override
+    public void onStatusChanged(int status) {
+
+    }
 
 }
--- app/src/main/java/de/htw_berlin/sharkandroidstack/sharkFW/protocols/wifidirect/WifiDirectStreamStub.java
+++ app/src/main/java/de/htw_berlin/sharkandroidstack/sharkFW/protocols/wifidirect/WifiDirectStreamStub.java
@@ -9,11 +9,9 @@ import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo;
 import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest;
 import android.os.Build;
 
-import net.sharkfw.kep.KEPStub;
 import net.sharkfw.protocols.RequestHandler;
 import net.sharkfw.protocols.StreamConnection;
 import net.sharkfw.protocols.StreamStub;
-import net.sharkfw.protocols.Stub;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -26,17 +24,30 @@ import java.util.Map;
  * Created by micha on 28.01.16.
  */
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-public class WifiDirectStreamStub implements StreamStub, WifiP2pManager.DnsSdTxtRecordListener {
+public class WifiDirectStreamStub implements StreamStub {
+
+    public static final int INITIATED = 0;
+    public static final int DISCOVERING = 1;
+    public static final int CONNECTED = 2;
+    public static final int STOPPED = 3;
+    public static final int DISCONNECTED = 4;
+
+    interface Status{
+        public void onStatusChanged(int status);
+    }
 
     private WifiP2pDnsSdServiceRequest serviceRequest;
     private Context context;
     private final WeakReference<Activity> activity;
     private RequestHandler handler;
-    private boolean isStarted = false;
 
+    private boolean isStarted = false;
     private WifiP2pManager manager;
     private WifiP2pManager.Channel channel;
     private Map<String, String> txtRecordMap;
+
+    private CommunicationManager communicationManager;
+//    private Status statusListener;
 
     public WifiDirectStreamStub(Context context, WeakReference<Activity> activity) {
         this.context = context;
@@ -49,16 +60,22 @@ public class WifiDirectStreamStub implements StreamStub, WifiP2pManager.DnsSdTxt
         this.txtRecordMap.put("entry0", getMACAddress());
         this.txtRecordMap.put("entry1", "This is just a test");
         this.txtRecordMap.put("entry2", "to check if discovering is working");
+        this.communicationManager = CommunicationManager.getInstance();
 
         WifiP2pDnsSdServiceInfo serviceInfo =
                 WifiP2pDnsSdServiceInfo.newInstance("_shark", "_presence._tcp", this.txtRecordMap);
 
         this.manager.addLocalService(this.channel, serviceInfo, new WifiActionListener("Add Local Service"));
 
+
         // TODO Set TxtListener to CommunicationManager
-        this.manager.setDnsSdResponseListeners(this.channel, null, this);
+        this.manager.setDnsSdResponseListeners(this.channel, null, communicationManager);
         this.serviceRequest = WifiP2pDnsSdServiceRequest.newInstance();
     }
+
+//    public void setStatusListener(Status statusListener) {
+//        this.statusListener = statusListener;
+//    }
 
     public String getMACAddress() {
         try {
@@ -91,6 +108,7 @@ public class WifiDirectStreamStub implements StreamStub, WifiP2pManager.DnsSdTxt
     public void stop() {
         this.manager.removeServiceRequest(
                 this.channel, serviceRequest, new WifiActionListener("Remove service request"));
+        communicationManager.onStatusChanged(STOPPED);
     }
 
     @Override
@@ -98,6 +116,7 @@ public class WifiDirectStreamStub implements StreamStub, WifiP2pManager.DnsSdTxt
         this.manager.addServiceRequest(
                 this.channel, serviceRequest, new WifiActionListener("Add service request"));
         this.manager.discoverServices(this.channel, new WifiActionListener("Discover services"));
+        communicationManager.onStatusChanged(DISCOVERING);
     }
 
     @Override
@@ -105,8 +124,4 @@ public class WifiDirectStreamStub implements StreamStub, WifiP2pManager.DnsSdTxt
         return isStarted;
     }
 
-    @Override
-    public void onDnsSdTxtRecordAvailable(String fullDomainName, Map<String, String> txtRecordMap, WifiP2pDevice srcDevice) {
-
-    }
 }
