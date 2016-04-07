98e70c116c452976e7c7ec77eee31b3d378473e9
Mario Neises <git@mnio.net>
2016-04-07 13:57:36 +0200

    working draft of nfc send cert

--- app/src/main/java/de/htw_berlin/sharkandroidstack/modules/pki/CertManagerListTab.java
+++ app/src/main/java/de/htw_berlin/sharkandroidstack/modules/pki/CertManagerListTab.java
@@ -8,7 +8,11 @@ import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import net.sharkfw.knowledgeBase.PeerSemanticTag;
+import net.sharkfw.knowledgeBase.inmemory.InMemoSharkKB;
+
 import de.htw_berlin.sharkandroidstack.R;
+import de.htw_berlin.sharkandroidstack.system_modules.log.LogManager;
 
 /**
  * Created by mn-io on 06.04.16.
@@ -28,6 +32,18 @@ public class CertManagerListTab extends RelativeLayout {
             @Override
             public void onClick(View v) {
                 Toast.makeText(v.getContext(), "Click", Toast.LENGTH_LONG).show();
+                PeerSemanticTag bob = InMemoSharkKB.createInMemoPeerSemanticTag("112663172666e296", "112663172666e296_Id", "tcp://112663172666e296");
+
+                String text;
+                try {
+                    PkiMainActivity.certManager.send(bob);
+                    text = "done";
+                } catch (Exception e) {
+                    text = "An error occurred: " + e.getMessage();
+                    LogManager.addThrowable(PkiMainActivity.LOG_ID, e);
+                }
+
+                Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
             }
         });
     }
